0eee68be86c0a48e8e9f0f8937e4fee918d71991
Hamroun Houssem <hamrounhoussem@gmail.com>
2021-03-07 10:52:17 +0100

    utilisation d'un objet C++ dans python

--- Animal/animal_component.cpp
+++ Animal/animal_component.cpp
@@ -1,14 +1,35 @@
 #include <pybind11/pybind11.h>
 
-char race[]="chat";
+char version[]="1.0";
 
-char const* getRace() {
-	return race;
+char const* getVersion() {
+	return version;
 }
 
-char const* jaiFaim() {
-  return "miaw... miaw... !";
+
+class Animal {    
+  private:            
+    int age;
+  public: 
+
+  
+Animal(int x){
+  age=x;
+  } 
+void setAge (int x) {
+ age = x;
+
 }
+int birthDay(){
+age+=1;
+return age;
+
+}
+
+
+};
+
+
 
 namespace py = pybind11;
 
@@ -16,7 +37,10 @@ namespace py = pybind11;
 PYBIND11_MODULE(animal_component,greetings)
 {
   greetings.doc() = "greeting_object 1.0";
-  
-  greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+      py::class_<Animal>(greetings, "Animal", py::dynamic_attr())
+        .def(py::init<int>())
+        .def("birthDay", &Animal::birthDay,"increment the age of the animal and return the new age")
+	.def("setAge", &Animal::setAge,"set the age of the animal");
 }
